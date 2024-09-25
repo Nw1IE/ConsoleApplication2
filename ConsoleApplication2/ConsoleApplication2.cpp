cada5a476cb44fb567dab716e2f8fb52531f92bb
Admin <Admin@DESKTOP-E7OIGPE>
2024-09-25 15:22:25 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    
+    int number;
+    int sum = 0;
+
+    std::cout << "Введите числа: " << std::endl;
+
+    while (true) {
+        std::cin >> number;
+
+        
+        if (number == 0) {
+            break;
+        }
+        sum += number;
+    }
+
+    std::cout << "Сумма всех чисел: " << sum << std::endl;
+
+    return 0;
+}
